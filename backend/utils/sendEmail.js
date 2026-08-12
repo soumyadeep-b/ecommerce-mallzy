5cd691e682fd3714a506475d49afa4cd5004da42
Soumyadeep Biswas <hisoumyadeepbiswas@gmail.com>
2026-08-13 00:17:08 +0530

    debug brevo

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,21 +1,8 @@
 const brevo = require('@getbrevo/brevo');
-
-const apiInstance = new brevo.TransactionalEmailsApi();
-apiInstance.authentications['apiKey'].apiKey = process.env.BREVO_API_KEY;
+console.log('BREVO EXPORTS:', Object.keys(brevo));
 
 const sendEmail = async ({ email, subject, message }) => {
-  try {
-    const sendSmtpEmail = new brevo.SendSmtpEmail();
-    sendSmtpEmail.sender = { name: 'Mallzy', email: 'hisoumyadeepbiswas@gmail.com' };
-    sendSmtpEmail.to = [{ email }];
-    sendSmtpEmail.subject = subject;
-    sendSmtpEmail.htmlContent = message;
-
-    await apiInstance.sendTransacEmail(sendSmtpEmail);
-    console.log(`Email successfully sent to ${email}`);
-  } catch (error) {
-    console.error(`Failed to send email to ${email}: ${error.message}`);
-  }
+  console.log('sendEmail called - debug mode, not sending yet');
 };
 
 module.exports = sendEmail;
